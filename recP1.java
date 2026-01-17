0177b1b0e317447796525a69da363b6b4bc07369
Vishal <Vishalsharma805966@gmail.com>
2026-01-17 16:27:22 +0530

    added recursion factorial code

---
+++ recP1.java
@@ -0,0 +1,38 @@
+public class recP1{
+  // public static void printDec(int n){
+  //   //Base case 
+  //   if(n==1){
+  //     System.out.println(n);
+  //     return;
+
+  //   }
+  //   System.out.println(n+" ");
+  //   printDec(n-1);
+  // }
+  // public static void printInc(int n){
+  //   //Base case
+  //   if(n==1){
+  //     System.out.println(n);
+  //     return;
+
+  //   }
+  //   printInc(n-1);
+  //   System.out.println(n+" ");
+  // }
+  public static int fact(int n){
+    if(n==0){
+      return 1;
+    }
+    return n*fact(n-1);
+
+
+  } 
+  public static void main(String[] args) {
+    int n=5;
+    // printDec(n);
+    // printInc(n);
+    System.out.println(fact(n));
+
+    
+  }
+} 
