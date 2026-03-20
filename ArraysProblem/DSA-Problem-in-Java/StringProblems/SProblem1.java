49c76e05e8a91b6f63ce096e13540fb31b9099c1
Creator Vishal <Vishalsharma805966@gmail.com>
2026-03-20 17:12:56 +0530

    Valid Palindrome

---
+++ ArraysProblem/DSA-Problem-in-Java/StringProblems/SProblem1.java
@@ -0,0 +1,35 @@
+public class SProblem1 {
+    public static boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+
+            // skip non-alphanumeric (left)
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+
+            // skip non-alphanumeric (right)
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            // compare (case insensitive)
+            if (Character.toLowerCase(s.charAt(left)) != 
+                Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome(s)); // true
+    }
+}
